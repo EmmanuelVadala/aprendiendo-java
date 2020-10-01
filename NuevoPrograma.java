b4556f8cb85c5fb35f44badd0e94c8059020d181
jyoti88 <72227448+jyoti88@users.noreply.github.com>
2020-10-02 01:51:39 +0530

    Update NuevoPrograma.java

--- NuevoPrograma.java
+++ NuevoPrograma.java
@@ -7,12 +7,12 @@ public class NuevoPrograma {
     public static void main(String[] args) {
        int num1=1;
        int num2=2;
-       int suma=0;
+       int sum=0;
        
-       suma=num1+num2;
-        System.out.println(suma);
+       sum=num1+num2;
+        System.out.println(sum);
     }
     
-    //lalalalalaala
+    
     
 }
