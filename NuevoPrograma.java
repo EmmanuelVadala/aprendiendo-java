b79d06f422836b4dfb5811f49f15c3d1e2fec700
EmmanuelVadala <Leonardo250186@gmail.com>
2020-10-01 17:03:39 -0300

    algo

---
+++ NuevoPrograma.java
@@ -0,0 +1,18 @@
+
+package nuevoprograma;
+
+
+public class NuevoPrograma {
+
+    public static void main(String[] args) {
+       int num1=1;
+       int num2=2;
+       int suma=0;
+       
+       suma=num1+num2;
+        System.out.println(suma);
+    }
+    
+    //lalalalalaala
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # aprendiendo-java
-xxxxxxx
+una frase
